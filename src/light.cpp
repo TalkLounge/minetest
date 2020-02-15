abce748d6f63e7415e85b82697bee0a22718aa01
TalkLounge <talklounge@yahoo.de>
2020-02-15 20:24:23 +0100

    Revert gamma 3

--- src/light.cpp
+++ src/light.cpp
@@ -40,9 +40,9 @@ void set_light_table(float gamma)
 	const float b = 3.0f - 2.0f * alpha - beta;
 	const float c = alpha;
 // Mid boost
-	const float boost = g_settings->getFloat("lighting_boost");
-	const float center = g_settings->getFloat("lighting_boost_center");
-	const float sigma = g_settings->getFloat("lighting_boost_spread");
+	const float d = g_settings->getFloat("lighting_boost");
+	const float e = g_settings->getFloat("lighting_boost_center");
+	const float f = g_settings->getFloat("lighting_boost_spread");
 // Gamma correction
 	gamma = rangelim(gamma, 0.5f, 3.0f);
 	
